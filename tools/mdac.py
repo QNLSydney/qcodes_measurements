c2ada8efec4c40eda957ff55921b97df2f867071
jana-d <djana@itp.phys.ethz.ch>
2018-04-27 17:03:59 +1000

    Update mdac wrappers to new firmware. Replace previous ramp function

--- tools/mdac.py
+++ tools/mdac.py
@@ -11,37 +11,11 @@ from qcodes.dataset.experiment_container import load_by_id
 import matplotlib
 import matplotlib.pyplot as plt
 
-def ramp_mdac(channel_voltage, voltage, rate=0.05, max_jump=0.002):
-    """
-    # TO DO: Use post_delay instead time.sleep() 
-    """
-
-    curr_v = channel_voltage()
-    diff_v = abs(curr_v - voltage)
-    
-    if diff_v > max_jump:
-        n_steps, remainder = divmod(diff_v, max_jump)
-        v_steps = np.linspace(curr_v, voltage, num=int(n_steps))
-        sleep_time = (diff_v / rate)/n_steps
-
-        for vv in v_steps:
-            time.sleep(sleep_time) # in seconds
-#             print(vv)
-            channel_voltage(vv)
-
-        if remainder != 0 and abs(channel_voltage() - voltage)< max_jump:
-            channel_voltage(voltage)
-        else:
-            channel_voltage(voltage)
-            # print("MDAC channel is not at desired voltage.")
-    else:
-        channel_voltage(voltage)
-
-def linear1d_ramp(mdac_channel_voltage, start, stop, num_points, delay, *param_meas):
+def linear1d_ramp(mdac_channel, start, stop, num_points, delay, *param_meas):
     """
     """
-    if abs(mdac_channel_voltage() - start) > 1e-6:
-        ramp_mdac(mdac_channel_voltage, start)
+    if abs(mdac_channel.voltage() - start) > 1e-6:
+        mdac_channel.ramp(start)
 
     try:
         elapsed_time = time.time()
@@ -50,25 +24,23 @@ def linear1d_ramp(mdac_channel_voltage, start, stop, num_points, delay, *param_m
 
         meas = Measurement()
         # register the first independent parameter
-        meas.register_parameter(mdac_channel_voltage)
+        meas.register_parameter(mdac_channel.voltage)
         output = []
 
         voltages = []
 
-        # mdac_channel_voltage.post_delay = delay
+        mdac_channel.voltage.post_delay = delay
 
         for parameter in param_meas:
-            meas.register_parameter(parameter, setpoints=(mdac_channel_voltage,))
+            meas.register_parameter(parameter, setpoints=(mdac_channel.voltage,))
             output.append([parameter, None])
 
         with meas.run() as datasaver:
             for set_point in np.linspace(start, stop, num_points):
-                ramp_mdac(mdac_channel_voltage, set_point)
-                # mdac_channel_voltage(set_point)
-                time.sleep(delay)
+                mdac_channel.ramp(set_point)
                 for i, parameter in enumerate(param_meas):
                     output[i][1] = parameter.get()
-                datasaver.add_result((mdac_channel_voltage, set_point),
+                datasaver.add_result((mdac_channel.voltage, set_point),
                                     *output)
         dataid = datasaver.run_id
         elapsed_time = time.time() - elapsed_time
@@ -79,41 +51,39 @@ def linear1d_ramp(mdac_channel_voltage, start, stop, num_points, delay, *param_m
 
     return dataid # can use plot_by_id(dataid)
 
-def linear2d_ramp(mdac_channel_v1, start1, stop1, num_points1, delay1,
-             mdac_channel_v2, start2, stop2, num_points2, delay2,
+def linear2d_ramp(mdac_channel1, start1, stop1, num_points1, delay1,
+             mdac_channel2, start2, stop2, num_points2, delay2,
              *param_meas):
 
-    if abs(mdac_channel_v1() - start1) > 1e-6:
-        ramp_mdac(mdac_channel_v1, start1)
+    if abs(mdac_channel1.voltage() - start1) > 1e-6:
+        mdac_channel1.ramp(start1)
 
-    if abs(mdac_channel_v2() - start2) > 1e-6:
-        ramp_mdac(mdac_channel_v2, start2)
+    if abs(mdac_channel2.voltage() - start2) > 1e-6:
+        mdac_channel2.ramp(start2)
     try:
         elapsed_time = time.time()
         param_meas = list(param_meas)
         _flush_buffers(*param_meas)
 
         meas = Measurement()
-        meas.register_parameter(mdac_channel_v1)
-        mdac_channel_v1.post_delay = delay1
-        meas.register_parameter(mdac_channel_v2)
-        mdac_channel_v2.post_delay = delay2
+        meas.register_parameter(mdac_channel1.voltage)
+        mdac_channel1.voltage.post_delay = delay1
+        meas.register_parameter(mdac_channel2.voltage)
+        mdac_channel2.voltage.post_delay = delay2
         output = []
         for parameter in param_meas:
-            meas.register_parameter(parameter, setpoints=(mdac_channel_v1, mdac_channel_v2))
+            meas.register_parameter(parameter, setpoints=(mdac_channel1.voltage, mdac_channel2.voltage))
             output.append([parameter, None])
 
         with meas.run() as datasaver:
             for set_point1 in np.linspace(start1, stop1, num_points1):
-                # param_set1.set(set_point1)
-                ramp_mdac(mdac_channel_v1, set_point1)
+                mdac_channel1.ramp(set_point1)
                 for set_point2 in np.linspace(start2, stop2, num_points2):
-                    # param_set2.set(set_point2)
-                    ramp_mdac(mdac_channel_v2, set_point2)
+                    mdac_channel2.ramp(set_point1)
                     for i, parameter in enumerate(param_meas):
                         output[i][1] = parameter.get()
-                    datasaver.add_result((mdac_channel_v1, set_point1),
-                                        (mdac_channel_v2, set_point2),
+                    datasaver.add_result((mdac_channel1.voltage, set_point1),
+                                        (mdac_channel2.voltage, set_point2),
                                         *output)
         dataid = datasaver.run_id
         elapsed_time = time.time() - elapsed_time
@@ -149,16 +119,19 @@ def plot_Wtext(dataid, mdac, fontsize=10, textcolor='black', textweight='normal'
     fig.subplots_adjust(right=0.75)
     
     if fig_folder is None:
-        fig_folder = os.getcwd()
+        fig_folder = os.path.join(os.getcwd(), 'figures')
 
-    fig.savefig(os.path.join(os.getcwd(),'figures', '{}.png'.format(dataid)))
+    if not os.path.exists(fig_folder):
+        os.makedirs(fig_folder)
 
-def change_filter_all(mdac):
+    fig.savefig(os.path.join(fig_folder, '{}.png'.format(dataid)))
+
+def change_filter_all(mdac, filter):
     """
     """
     print("Setting all voltages to zero.")
     for channel in mdac.channels:
-        ramp_mdac(channel.voltage, 0)
+        channel.ramp(0)
         channel.filter(2)
 
 def all_to_smc(mdac, command='close'):
@@ -166,7 +139,7 @@ def all_to_smc(mdac, command='close'):
     """
     print("Setting all voltages to zero.")
     for channel in mdac.channels:
-        ramp_mdac(channel.voltage, 0)
+        channel.ramp(0)
         channel.smc(command)
 
 def all_to_microd(mdac, command='close'):
@@ -174,15 +147,15 @@ def all_to_microd(mdac, command='close'):
     """
     print("Setting all voltages to zero.")
     for channel in mdac.channels:
-        ramp_mdac(channel.voltage, 0)
+        channel.ramp(0)
         channel.microd(command)
 
 def all_to_zero(mdac):
     for channel in mdac.channels:
-        ramp_mdac(channel.voltage, 0)
+        channel.ramp(0)
 
 def ground_all(mdac):
     for ch in mdac.channels:
-        ramp_mdac(ch.voltage, 0)
+        channel.ramp(0)
         ch.dac_output('open')
         ch.gnd('close')
