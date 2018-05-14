80b4a06f9d063c890108ba8198479f9e286dc6a3
Sebastian Pauka <spauka@gmail.com>
2018-05-14 11:48:38 +1000

    Add remote functions

---
+++ plot/rpyplot.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+from math import ceil
+import warnings
+
+from pyqtgraph import *
+from pyqtgraph.exporters import *
+
+from numpy import linspace, min, max, ndarray
+
+class DraggableTextItem(GraphicsWidget, GraphicsWidgetAnchor):
+    def __init__(self, text="", offset=None, *args, **kwargs):
+        GraphicsWidget.__init__(self)
+        GraphicsWidgetAnchor.__init__(self)
+        self.setFlag(self.ItemIgnoresTransformations)
+        self.layout = QtGui.QGraphicsGridLayout()
+        self.setLayout(self.layout)
+        self.item_anchor = (0, 0)
+        self.object_anchor = (0, 0)
+        if offset is None:
+            self.offset = (0, 0)
+        else:
+            self.offset = offset
+
+        self.label_item = LabelItem()
+        self.label_item.setText(text)
+        self.layout.addItem(self.label_item, 0, 0)
+
+        self.updateSize()
+
+    def setParentItem(self, p):
+        ret = GraphicsWidget.setParentItem(self, p)
+        if self.offset is not None:
+            offset = Point(self.offset)
+            anchorx = 1 if offset[0] <= 0 else 0
+            anchory = 1 if offset[1] <= 0 else 0
+            anchor = (anchorx, anchory)
+            self.anchor(itemPos=anchor, parentPos=anchor, offset=offset)
+        return ret
+
+    def paint(self, p, *args):
+        p.setPen(fn.mkPen(255,255,255,100))
+        p.setBrush(fn.mkBrush(100,100,100,50))
+        p.drawRect(self.boundingRect())
+
+    def anchor(self, itemPos, parentPos, offset=(0, 0)):
+        super().anchor(itemPos, parentPos, offset)
+        self.item_anchor = itemPos
+        self.object_anchor = parentPos
+        self.offset = offset
+
+    def getOffset(self):
+        return self.offset
+    def setOffset(self, offs):
+        if not isinstance(offs, tuple) or len(offs) != 2:
+            raise ValueError("Must be a tuple (x, y)")
+        self.anchor(self.item_anchor, self.object_anchor, offs)
+
+    def updateSize(self):
+        self.setGeometry(0, 0, self.label_item.width()+10, self.label_item.height())
+    def boundingRect(self):
+        return QtCore.QRectF(0, 0, self.width(), self.height())
+
+    def hoverEvent(self, ev):
+        ev.acceptDrags(QtCore.Qt.LeftButton)
+    def mouseDragEvent(self, ev):
+        if ev.button() == QtCore.Qt.LeftButton:
+            dpos = ev.pos() - ev.lastPos()
+            self.autoAnchor(self.pos() + dpos)
+
+    def setText(self, text):
+        self.label_item.setText(str(text))
+        self.updateSize()
+    def getText(self):
+        return self.label_item.text
+
+class ExtendedPlotItem(PlotItem):
+    def export(self, fname, export_type="image"):
+        """
+        Save the item as an image
+        """
+        if export_type == "image":
+            exporter = ImageExporter(self)
+        elif export_type == "svg":
+            exporter = SVGExporter(self)
+        exporter.export(fname)
+        del exporter
+
+class ExtendedPlotWindow(GraphicsLayoutWidget):
+    def export(self, fname, export_type="image"):
+        """
+        Save the item as an image
+        """
+        if export_type == "image":
+            exporter = ImageExporter(self.scene())
+        elif export_type == "svg":
+            exporter = SVGExporter(self.scene())
+        exporter.export(fname)
+        del exporter
