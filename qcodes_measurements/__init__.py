602299f97ae4c0957175420528039dafbad388b6
Sebastian Pauka <spauka@gmail.com>
2018-05-29 11:32:15 +1000

    Add check for MDAC drivers.
    Don't load the MDAC module if the MDAC driver is not
    present on the system.

--- qcodes_measurements/__init__.py
+++ qcodes_measurements/__init__.py
@@ -1,10 +1,24 @@
 from .plot import ChildProcessImportError
+import logging
+
+__all__ = ['tools', 'plot']
+
+logger = logging.getLogger("qcodes_measurements")
+
 try:
+    # Import shortcuts to plot tools
     from .plot import plot_tools, pyplot
 
+    # Import shortcuts to measurements
     from .tools.measure import linear1d, linear2d
-    from .tools.mdac import *
-    from .tools.parameters import FilterWrapper, CutWrapper
+    from .tools.parameters import FilterWrapper, CutWrapper, SmoothFilter, DiffFilter
+
+    # If we have an MDAC, import MDAC shortcuts
+    try:
+        import qcodes.instrument_drivers.qnl.MDAC as MDAC
+        from .tools.mdac import *
+    except ModuleNotFoundError:
+        logger.info("MDAC drivers not present. Not loading drivers")
 except ChildProcessImportError:
     # Don't do this if we are the child process
     pass
