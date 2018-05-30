fc95fe64f0ab25f549ba5f031125c86a97bda57c
Sebastian Pauka <spauka@gmail.com>
2018-05-30 13:46:34 +1000

    Add expand functionality to plots

--- qcodes_measurements/plot/rpyplot.py
+++ qcodes_measurements/plot/rpyplot.py
@@ -2,12 +2,15 @@
 
 from math import ceil
 import warnings
+from functools import partial
 
 from pyqtgraph import *
 from pyqtgraph.exporters import *
 import pyqtgraph.multiprocess as mp
+from pyqtgraph.GraphicsScene.mouseEvents import MouseClickEvent
 
-from PyQt5.QtWidgets import QApplication, QMessageBox, QMainWindow, QAction
+from PyQt5.QtWidgets import QApplication, QMessageBox, QMainWindow, QAction, QGraphicsSceneMouseEvent
+from PyQt5 import QtCore
 
 from numpy import linspace, min, max, ndarray
 
@@ -77,7 +80,114 @@ class DraggableTextItem(GraphicsWidget, GraphicsWidgetAnchor):
     def getText(self):
         return self.label_item.text
 
+class CustomViewBox(ViewBox):
+    def __init__(self, *args, **kwargs):
+        # Initialize the superclass
+        super().__init__(*args, **kwargs)
+
+        # Add extra actions to the context menu
+        self.rbActions = (
+            ('Expand', partial(self.expand, axis='XY')),
+            ('Expand X', partial(self.expand, axis='X')),
+            ('Expand Y', partial(self.expand, axis='Y')),
+        )
+
+        # And set up the box menu
+        self.plainMenu = self.menu
+        self.rbMenu = ViewBoxMenu.ViewBoxMenu(self)
+
+        # And add in our custom actions
+        firstAction = self.rbMenu.actions()[0]
+        for action in self.rbActions:
+            qaction = QtGui.QAction(action[0], self.rbMenu)
+            qaction.triggered.connect(action[1])
+            self.rbMenu.insertAction(firstAction, qaction)
+        self.rbMenu.insertSeparator(firstAction)
+
+    def sceneEventFilter(self, obj, ev):
+        if obj == self.rbScaleBox:
+            # Run our custom handler if we've clicked inside the scale box
+            if isinstance(ev, QGraphicsSceneMouseEvent):
+                try:
+                    # We only handle left button events inside the rectangle
+                    if (ev.button() & QtCore.Qt.LeftButton) and obj.rect().contains(ev.pos()):
+                        # Check for a mouse click inside the scale box on the left mouse button
+                        # Ignore events where we've dragged.
+                        if ((ev.type() == QtCore.QEvent.GraphicsSceneMouseRelease) and
+                            (ev.buttonDownPos(QtCore.Qt.LeftButton) == ev.pos())):
+                            # Translate event to PyQtGraph event framework
+                            ev = MouseClickEvent(ev)
+                            ev.accept()
+
+                            # Get the menu
+                            self.menu = self.rbMenu
+                            # Then add in context dependant menus
+                            self.scene().addParentContextMenus(self, self.menu, ev)
+                            # And finally raise the context menu
+                            self.raiseContextMenu(ev)
+                            return True
+                        elif (ev.type() == QtCore.QEvent.GraphicsSceneMousePress):
+                            return True
+                except Exception as e:
+                    print(e)
+        return False
+
+    def mouseClickEvent(self, ev):
+        if (ev.button() & QtCore.Qt.LeftButton):
+            ev.accept()
+            self.rbScaleBox.hide()
+            self.menu = self.plainMenu
+            return True
+        return super().mouseClickEvent(ev)
+
+    def mouseDragEvent(self, ev, axis=None):
+        # Accept the event
+        ev.accept()
+
+        # Draw the box
+        if ev.button() & QtCore.Qt.LeftButton:
+            self.updateScaleBox(ev.buttonDownPos(), ev.pos())
+            self.rbScaleBox.removeSceneEventFilter(self)
+            self.rbScaleBox.installSceneEventFilter(self)
+            return True
+        return super().mouseDragEvent(ev)
+
+    def expand(self, axis='XY'):
+        # Get the size of the scale box
+        p = self.rbScaleBox.mapToParent(self.rbScaleBox.rect())
+        p1, p2 = p[0], p[2]
+        rect = self.viewRect()
+        if 'X' not in axis:
+            p1.setX(rect.getCoords()[0])
+            p2.setX(rect.getCoords()[2])
+        if 'Y' not in axis:
+            p1.setY(rect.getCoords()[1])
+            p2.setY(rect.getCoords()[3])
+        ax = QtCore.QRectF(p1, p2)
+        # Hide the scale box
+        self.rbScaleBox.hide()
+        # Reset the menu
+        self.menu = self.plainMenu
+        # Do scale
+        self.setRange(rect=ax, padding=0)
+        self.axHistoryPointer += 1
+        self.axHistory = self.axHistory[:self.axHistoryPointer] + [ax]
+
+
 class ExtendedPlotItem(PlotItem):
+    def __init__(self, *args, **kwargs):
+        """
+        Create a new PlotItem, same as a base plotitem, but with a few
+        extra pieces of functionality.
+          - Export Image
+          - Keep track of images and allow proxying
+          - Use a custom view box, such that we can do common tasks
+        """
+        if 'viewBox' not in kwargs:
+            vb = CustomViewBox()
+            kwargs['viewBox'] = vb
+        super().__init__(*args, **kwargs)
+
     def export(self, fname, export_type="image"):
         """
         Save the item as an image
@@ -104,7 +214,6 @@ class ExtendedPlotItem(PlotItem):
         data_items = super().listDataItems()
         data_items = [mp.proxy(item) for item in data_items]
         return data_items
-
 
 class ExtendedPlotWindow(GraphicsLayoutWidget):
     _windows = []
