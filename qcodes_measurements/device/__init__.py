70e405421dfa927bb8b1f35c174b40a80ca15100
Sebastian Jun Pauka <spau4795@SHARED.SYDNEY.EDU.AU>
2018-11-21 12:24:03 +1100

    Missed removing BB37

--- qcodes_measurements/device/__init__.py
+++ qcodes_measurements/device/__init__.py
@@ -1,7 +1,6 @@
 from .device import Device
 from .gate import Gate, GateWrapper, MDACGateWrapper, Ohmic, OhmicWrapper, MDACOhmicWrapper
-from .bb import BB, BBChan
-from .bb37 import BB37, BB37Chan
+from .bb import BB, BBChan, BB37, BB37Chan
 from .feedback import Feedback
 
 __all__ = ["BB", "BBChan", "BB37", "BB37Chan", "Device", "Gate", "GateWrapper",
