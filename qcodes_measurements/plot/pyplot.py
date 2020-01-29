a114c048fa5d1e587fcce836ae03ab266ba2f627
Sebastian Pauka <s.pauka@sydney.edu.au>
2020-01-30 09:54:15 +1100

    Correctly cache update in image

--- qcodes_measurements/plot/pyplot.py
+++ qcodes_measurements/plot/pyplot.py
@@ -600,6 +600,7 @@ class ExtendedPlotDataItem(PlotDataItem):
     def __init__(self, setpoint_x, *args, **kwargs):
         super().__init__(setpoint_x, *args, **kwargs)
         self.setpoint_x = _ensure_ndarray(setpoint_x)
+        self._remote_function_options['update'] = {'callSync': 'off'}
 
     def __wrap__(self, *args, **kwargs):
         super().__wrap__(*args, **kwargs)
@@ -609,7 +610,7 @@ class ExtendedPlotDataItem(PlotDataItem):
 
     @property
     def setpoint_x(self):
-        return self.setpoint_x
+        return self._base_inst.setpoint_x
     @setpoint_x.setter
     def setpoint_x(self, val):
         self._base_inst.setpoint_x = _ensure_ndarray(val)
@@ -625,7 +626,7 @@ class ExtendedPlotDataItem(PlotDataItem):
         self._base_inst.setpoint_x = val
 
     def update(self, data, *args, **kwargs):
-        self._base_inst.update(data)
+        RPGWrappedBase.__getattr__(self, "update")(data)
 
 class ImageItem(PlotData):
     _base = "ImageItem"
