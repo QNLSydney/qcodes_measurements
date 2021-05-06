ad80bb16483684c39f22993dff6e727f82c387fb
Sebastian Pauka <v-spauka@microsoft.com>
2021-05-06 16:00:48 +1000

    Set cache age on fridge temps

--- qcodes_measurements/drivers/fridge.py
+++ qcodes_measurements/drivers/fridge.py
@@ -9,7 +9,7 @@ class FridgeTemps(Instrument):
     """
     Instrument definition for a fridge logged on the thermometry site.
     """
-    def __init__(self, name, url):
+    def __init__(self, name, url, refresh_interval=60):
         super().__init__(name)
         self.url = url
 
@@ -26,13 +26,21 @@ class FridgeTemps(Instrument):
                                unit="K",
                                label=f"{param}",
                                get_cmd=partial(self.get_param, param),
-                               snapshot_get=True)
+                               snapshot_get=True,
+                               max_val_age=refresh_interval)
 
     def get_param(self, param):
         temps = requests.get(self.url)
         if temps.status_code != 200:
             raise RuntimeError("Unable to query fridge")
         temps = temps.json()
+
+        # Update all parameter cache values
+        for p in self.params:
+            if p in temps:
+                if f"{p}_temp" in self.parameters:
+                    self.parameters[f"{p}_temp"].cache.set(temps[p])
+        # Return the current value
         return temps[param]
 
     def ask_raw(self, cmd):
