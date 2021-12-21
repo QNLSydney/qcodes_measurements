d4fc5b0937f440bd043a5031ab4f6b81c84292bd
Sebastian Pauka <sebastian.pauka@sydney.edu.au>
2021-12-21 17:27:22 +1100

    Fix another broken Qt5 import

--- qcodes_measurements/plot/remote/DraggableTextItem.py
+++ qcodes_measurements/plot/remote/DraggableTextItem.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 from pyqtgraph import GraphicsWidget, GraphicsWidgetAnchor, LabelItem, Point, mkPen, mkBrush
 
@@ -10,7 +10,7 @@ class DraggableTextItem(GraphicsWidget, GraphicsWidgetAnchor):
         GraphicsWidget.__init__(self)
         GraphicsWidgetAnchor.__init__(self)
         self.setFlag(self.ItemIgnoresTransformations)
-        self.layout = QtGui.QGraphicsGridLayout()
+        self.layout = QtWidgets.QGraphicsGridLayout()
         self.setLayout(self.layout)
         self.item_anchor = (0, 0)
         self.object_anchor = (0, 0)
