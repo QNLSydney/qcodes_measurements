26a6413350a2565f459578c3b185fc08b4bbc789
Sebastian Pauka <Sebastian.Pauka@sydney.edu.au>
2025-10-09 18:44:59 +1100

    Fix type errors in logging

--- qcodes_measurements/logging.py
+++ qcodes_measurements/logging.py
@@ -2,48 +2,53 @@ import io
 import os
 import sys
 import logging
+from typing import Optional
 
 __all__ = ["LoggingStream", "get_logger", "set_log_level"]
 
 # Get access to module level variables
-this = sys.modules[__name__]
+logger: Optional[logging.Logger] = None
 
-def get_logger(name=None, debug=False):
+def get_logger(name=None, debug=False) -> logging.Logger:
     # Disable logging to stderr and capture warnings
     logging.lastResort = None
     logging.captureWarnings(True)
 
     # Create a root logger the first time this module is called
-    if getattr(this, "logger", None) is None:
-        this.logger = logging.getLogger("qcm")
+    if logger is None:
+        local_logger = logging.getLogger("qcm")
+        globals()["logger"] = logging.getLogger("qcm")
+
         if debug:
-            this.logger.setLevel(logging.DEBUG)
+            local_logger.setLevel(logging.DEBUG)
         else:
-            this.logger.setLevel(logging.INFO)
+            local_logger.setLevel(logging.INFO)
         # Create a handler for the logger
         log_handler = logging.FileHandler("rpyplot.log")
         log_handler.setLevel(logging.DEBUG)
         log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         log_handler.setFormatter(log_format)
-        this.logger.addHandler(log_handler)
+        local_logger.addHandler(log_handler)
+    else:
+        local_logger = logger
 
     # Get the requested logger
     if "QCM_REMOTE" in os.environ:
         base = f"remote_{os.getpid()}"
         if name is None:
-            return this.logger.getChild(base)
-        return this.logger.getChild(f"{base}.{name}")
+            return local_logger.getChild(base)
+        return local_logger.getChild(f"{base}.{name}")
     if name is None:
-        return this.logger
-    return this.logger.getChild(name)
+        return local_logger
+    return local_logger.getChild(name)
 
 def set_log_level(level="INFO", name=None):
     """
     Set the log level for a given module (name) to the level.
     """
     # Get the logger
-    logger = get_logger(name)
-    logger.setLevel(level)
+    local_logger = get_logger(name)
+    local_logger.setLevel(level)
     get_logger().debug(f"Set log level of %r to %r", logger, level)
 
 class LoggingStream(io.IOBase):
