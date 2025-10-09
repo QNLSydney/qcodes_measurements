63b36defd71c3f35de74696223a357dbfdf27b27
Sebastian Pauka <Sebastian.Pauka@sydney.edu.au>
2025-10-09 17:14:35 +1100

    Fix remote object wrapping for new python

--- qcodes_measurements/plot/local/RemoteProcessWrapper.py
+++ qcodes_measurements/plot/local/RemoteProcessWrapper.py
@@ -14,6 +14,7 @@ logger = get_logger("RPGWrapper")
 
 __all__ = ["ensure_ndarray", "auto_wrap", "RPGWrappedBase", "get_remote"]
 
+
 def ensure_ndarray(array):
     """
     Ensure the given array is a numpy array. Necessary for some parts of pyqtgraph.
@@ -24,26 +25,31 @@ def ensure_ndarray(array):
         return np.array(array)
     return array
 
+
 def auto_wrap(f):
     """
     Decorator to ensure values are wrapped by RPGWrappedBase
     """
+
     def wrap(*args, **kwargs):
         val = f(*args, **kwargs)
         if val is None:
             return val
         return RPGWrappedBase.autowrap(val)
+
     return wrap
+
 
 def _set_defaults(remote):
     """
     Set up the default state of the plot windows. Add any other global config options here.
     """
-    remote.setConfigOption('background', 'w')
-    remote.setConfigOption('foreground', 'k')
-    remote.setConfigOption('leftButtonPan', False)
-    remote.setConfigOption('antialias', True)
+    remote.setConfigOption("background", "w")
+    remote.setConfigOption("foreground", "k")
+    remote.setConfigOption("leftButtonPan", False)
+    remote.setConfigOption("antialias", True)
     remote._setProxyOptions(deferGetattr=False)
+
 
 def start_remote():
     # Check that a QApplication has been created
@@ -53,9 +59,10 @@ def start_remote():
         this.app = pyqtgraph.Qt.QtWidgets.QApplication.instance()
 
     this.proc = QtProcess(debug=False)
-    this.rpg = this.proc._import('qcodes_measurements.plot.rpyplot', timeout=20)
+    this.rpg = this.proc._import("qcodes_measurements.plot.rpyplot", timeout=20)
     this.rbuiltins = this.proc._import("builtins")
     _set_defaults(this.rpg)
+
 
 def restart_remote():
     if len(QtProcess.handlers) == 0:
@@ -72,8 +79,10 @@ def restart_remote():
         QtProcess.handlers.clear()
         start_remote()
 
+
 def get_remote():
     return this.rpg
+
 
 def remote_callable(remote_obj):
     # If the object is local, shortcut to the local callable
@@ -95,7 +104,7 @@ class RPGWrappedBase(ObjectProxy):
     _remote_functions = None
     _remote_function_options = None
 
-    def __init__(self, *args, **kwargs): # pylint: disable=super-init-not-called
+    def __init__(self, *args, **kwargs):  # pylint: disable=super-init-not-called
         self._remote_functions = {}
         self._remote_function_options = {}
 
@@ -105,16 +114,20 @@ class RPGWrappedBase(ObjectProxy):
         if this.rpg._handler.proc.is_alive() is False:
             restart_remote()
 
-        if '_base' in self.__class__.__dict__:
+        if "_base" in self.__class__.__dict__:
             base = getattr(this.rpg, self.__class__._base)
             base = base(*args, **kwargs)
             self._base_inst = base
         else:
-            raise TypeError("Base instance not defined. Don't know how to create remote object.")
+            raise TypeError(
+                "Base instance not defined. Don't know how to create remote object."
+            )
 
     def __wrap__(self, *args, **kwargs):
         if args or kwargs:
-            raise TypeError(f"RPGWrappedBase.__wrap__ expects no arguments. Got args={args}, kwargs={kwargs}")
+            raise TypeError(
+                f"RPGWrappedBase.__wrap__ expects no arguments. Got args={args}, kwargs={kwargs}"
+            )
         self._remote_functions = {}
         self._remote_function_options = {}
         # And make sure that ndarrays are still proxied
@@ -127,13 +140,12 @@ class RPGWrappedBase(ObjectProxy):
         # Create an empty instance of RPGWrappedBase,
         # and copy over instance variables
         base_inst = cls.__new__(cls)
-        base_inst.__dict__ = {**base_inst.__dict__,
-                              **instance.__dict__}
+        base_inst.__dict__ = {**base_inst.__dict__, **instance.__dict__}
         base_inst._base_inst = instance
 
         # If we do want to initialize some instance variables, we can do it in
         # the special __wrap__ method
-        __wrap__ = getattr(base_inst, '__wrap__', None)
+        __wrap__ = getattr(base_inst, "__wrap__", None)
         if __wrap__ is not None:
             __wrap__(*args, **kwargs)
 
@@ -146,38 +158,55 @@ class RPGWrappedBase(ObjectProxy):
         if RPGWrappedBase._subclass_types is None:
             logger.debug("Populating subclass types")
             RPGWrappedBase._subclass_types = {}
+
             def append_subclasses(sc_dict, cls):
                 for typ in cls.__subclasses__():
                     append_subclasses(sc_dict, typ)
-                    base = getattr(typ, '_base', None)
+                    base = getattr(typ, "_base", None)
                     if base is None:
                         continue
                     typestr = base
                     sc_dict[typestr] = typ
+
             append_subclasses(RPGWrappedBase._subclass_types, RPGWrappedBase)
 
         # Then, if we have an object proxy, wrap it if it is in the list of wrappable types
         if isinstance(inst, ObjectProxy):
             if isinstance(inst, RPGWrappedBase):
-                logger.debug("Object already wrapped. Has types: %r.", inst.__class__.__mro__)
+                logger.debug(
+                    "Object already wrapped. Has types: %r.", inst.__class__.__mro__
+                )
                 return inst
 
+            # Get name of remote type
+            remote_type = inst._getSpecialAttr('__class__').__name__
+
             # Check if we have a list of objects:
-            if getattr(inst, "__class__").__name__ in ("tuple", "list"):
+            if remote_type in ("tuple", "list"):
                 logger.debug("Wrapping remote list.")
                 # Need to iterate over things in a dumb way to suppress warnings
                 return tuple(RPGWrappedBase.autowrap(inst[i]) for i in range(len(inst)))
 
+            # Try to match the remote type
+            logger.debug("Extracted remote type: %s.", remote_type)
+            if remote_type in RPGWrappedBase._subclass_types:
+                return RPGWrappedBase._subclass_types[remote_type].wrap(inst)
+
             # Otherwise look to see if we have an extended type
-            logger.debug("Matching %s", inst._typeStr)
-            typestr = re.match(r"^<[a-zA-Z_.]+\.([a-zA-Z_]+)(?:\(.*\))? at 0x[0-9A-Fa-f]+>$", inst._typeStr)
+            logger.debug("Fallback matching %s", inst._typeStr)
+            typestr = re.match(
+                r"^<[a-zA-Z_.]+\.([a-zA-Z_]+)(?:\(.*\))? (?:object )?at 0x[0-9A-Fa-f]+>$",
+                str(inst._typeStr),
+            )
             if typestr:
                 logger.debug("Extracted remote type: %s.", typestr.groups()[0])
                 typestr = typestr.groups()[0]
                 if typestr in RPGWrappedBase._subclass_types:
                     return RPGWrappedBase._subclass_types[typestr].wrap(inst)
         else:
-            logger.debug("Object is not an ObjectProxy. Has types: %r.", inst.__class__.__mro__)
+            logger.debug(
+                "Object is not an ObjectProxy. Has types: %r.", inst.__class__.__mro__
+            )
         # Otherwise, just return the bare instance
         return inst
 
@@ -194,12 +223,13 @@ class RPGWrappedBase(ObjectProxy):
             # Try to translate to one of the friendly names
             res = RPGWrappedBase.autowrap(res)
             return res
+
         return save
 
     def wrap_getters(self, f):
         return auto_wrap(f)
 
-    def __setattr__(self, name, val, **kwargs): # pylint: disable=arguments-differ
+    def __setattr__(self, name, val, **kwargs):  # pylint: disable=arguments-differ
         for cls in self.__class__.__mro__:
             if name in cls.__dict__:
                 object.__setattr__(self, name, val)
@@ -227,17 +257,32 @@ class RPGWrappedBase(ObjectProxy):
             for cls in self.__class__.__mro__:
                 if name in cls.__dict__:
                     v = cls.__dict__[name]
-                    if hasattr(v, '__get__'):
+                    if hasattr(v, "__get__"):
                         attr = v.__get__(None, self)
-                        logger.debug("Found attr %s locally in subclass %r, and is a property. Returns value: %r.", name, cls, attr)
+                        logger.debug(
+                            "Found attr %s locally in subclass %r, and is a property. Returns value: %r.",
+                            name,
+                            cls,
+                            attr,
+                        )
                     else:
                         attr = v
-                        logger.debug("Found attr %s locally in subclass %r. Returns value: %r.", name, cls, attr)
+                        logger.debug(
+                            "Found attr %s locally in subclass %r. Returns value: %r.",
+                            name,
+                            cls,
+                            attr,
+                        )
                     break
             else:
                 if name in self._base_inst.__dict__:
                     attr = self._base_inst.__dict__[name]
-                    logger.debug("Found attr %s locally in base_inst (%r). Returns value: %r", name, self._base_inst, attr)
+                    logger.debug(
+                        "Found attr %s locally in base_inst (%r). Returns value: %r",
+                        name,
+                        self._base_inst,
+                        attr,
+                    )
         # Otherwise check whether the attribute exists on the remote
         if search_location in ("remote", "both") and attr is None:
             logger.debug("Looking for attr %s remotely.", name)
@@ -251,7 +296,9 @@ class RPGWrappedBase(ObjectProxy):
 
         # If we didn't find the attribute in either location, raise an AttributeError
         if attr is None:
-            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+            raise AttributeError(
+                f"'{self.__class__.__name__}' object has no attribute '{name}'"
+            )
 
         # Check if item is a wrappable type
         attr = self.autowrap(attr)
